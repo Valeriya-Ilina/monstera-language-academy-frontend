11d36df9075f77a7feff98f54e7941dddb5145c9
Valeriya Ilina <59905206+Valeriya-Ilina@users.noreply.github.com>
2021-04-18 21:10:18 -0400

    login and signup pages are rendered

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,13 +1,68 @@
 import React, { Component } from 'react'
+import { Form, Button } from 'react-bootstrap'
 
 class Login extends Component {
   constructor(props) {
     super(props)
+
+    this.state = {
+      url: 'http://localhost:3060/sessions',
+      username: '',
+      password: '',
+      response: ''
+    }
+  }
+
+  handleChange = (event) => {
+    this.setState({ [event.target.id] : event.target.value })
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault()
+
+    const data = {
+      username: this.state.username,
+      password: this.state.password
+    }
+
+    fetch(this.state.url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    })
+      .then(response => {
+          return response.json()
+      })
+      .then(json => this.setState({
+          repsonse: json.data,
+          username: '',
+          password: ''
+        }),
+        err => console.log(err))
   }
 
   render() {
+    console.log(this.state)
     return(
-      <h1>This is Login Form</h1>
+      <div id="login-container">
+        <h1>Login</h1>
+        <Form onSubmit={this.handleSubmit}>
+          <Form.Group controlId="username">
+            <Form.Label>Username</Form.Label>
+            <Form.Control type="text" placeholder="Username" onChange={this.handleChange} />
+          </Form.Group>
+
+          <Form.Group controlId="password">
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" placeholder="Password" onChange={this.handleChange} />
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Login
+          </Button>
+        </Form>
+      </div>
     )
   }
 }
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,13 +1,77 @@
 import React, { Component } from 'react'
+import { Form, Button, InputGroup } from 'react-bootstrap'
 
 class Signup extends Component {
   constructor(props) {
     super(props)
+
+    this.state = {
+      url: 'http://localhost:3060/users',
+      username: '',
+      password: '',
+      response: '',
+    }
+  }
+
+  handleChange = (event) => {
+    this.setState({ [event.target.id] : event.target.value })
+  }
+
+  handleSubmit = (event) => {
+
+    const data = {
+      username: this.state.username,
+      password: this.state.password
+    }
+
+    fetch(this.state.url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    })
+      .then(response => {
+          return response.json()
+      })
+      .then(json => this.setState({
+          repsonse: json.data,
+          username: '',
+          password: ''
+        }),
+        err => console.log(err))
   }
 
   render() {
     return(
-      <h1>This is Sign Up Form</h1>
+      <div id="signup-container">
+        <h1>Signup</h1>
+        <Form onSubmit={this.handleSubmit}>
+          <Form.Group controlId="username">
+            <Form.Label>Username</Form.Label>
+            <Form.Control
+              required
+              type="text"
+              placeholder="Username"
+              onChange={this.handleChange}
+            />
+          </Form.Group>
+
+          <Form.Group controlId="password">
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              required
+              type="password"
+              placeholder="Password"
+              onChange={this.handleChange}
+            />
+          </Form.Group>
+
+          <Button variant="primary" type="submit">
+            Create User
+          </Button>
+        </Form>
+      </div>
     )
   }
 }
