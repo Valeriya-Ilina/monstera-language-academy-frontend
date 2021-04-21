f41b6aa2c9d002eb2071dc775edba7622a1e198b
Valeriya Ilina <59905206+Valeriya-Ilina@users.noreply.github.com>
2021-04-21 01:04:27 -0400

    remove keyboard component

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,7 +2,6 @@ import { Component } from 'react'
 import Search from './Search'
 import Glossary from './Glossary'
 import PracticeInfo from './PracticeInfo'
-import Keyboard from './Keyboard'
 import FlashCard from './FlashCard'
 
 class Main extends Component {
@@ -15,7 +14,6 @@ class Main extends Component {
       <>
         <div id="search-container">
           <Search />
-          <Keyboard />
         </div>
         <div id="learn-container">
           <Glossary />
