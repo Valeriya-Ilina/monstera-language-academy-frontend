baa64fabbbaa06d162e916a482485a5df76e4dad
Valeriya Ilina <59905206+Valeriya-Ilina@users.noreply.github.com>
2021-04-21 01:06:52 -0400

    search component is working

--- src/components/Search.js
+++ src/components/Search.js
@@ -6,14 +6,11 @@ class Search extends Component {
     super(props)
 
     this.state = {
-      baseURL: "",
-      apikey: "",
-      query: "",
+      baseURL: "http://localhost:3060/search",
       searchWordText: "",
-      translateFrom: "",
-      translateTo: "",
-      seacrhURL: '',
-      translationData: []
+      translateFrom: "en",
+      translateTo: "ru",
+      translationData: {}
     }
   }
 
@@ -21,45 +18,82 @@ class Search extends Component {
     this.setState({ [event.target.id] : event.target.value })
   }
 
-  handleSubmit = (event) => {
+  handleSubmit = async (event) => {
     event.preventDefault()
 
     // TODO set default values in the dropdown
     console.log(this.state.translateFrom)
     console.log(this.state.translateTo)
+    console.log(this.state.searchWordText)
+
+    const url = this.state.baseURL
+    try {
+      const response = await fetch(url, {
+        method: 'POST',
+        body: JSON.stringify({
+          translateTo: this.state.translateTo,
+          text: this.state.searchWordText
+        }),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      })
+
+      let body = await response.json()
+      if (response.status === 200) {
+        this.setState({
+          translationData: body
+        })
+      }
+    }
+    catch(err){
+      console.log('Error => ', err);
+    }
   }
+
   render() {
+    console.log(this.state)
     return(
-      <Form onSubmit={this.handleSubmit}>
-        <Form.Group controlId="translateFrom">
-        <Form.Label>Translate From</Form.Label>
-          <Form.Control as="select" size="sm" onChange={this.handleChange} custom>
-            <option selected>English</option>
-            <option>Italian</option>
-          </Form.Control>
-        </Form.Group>
+      <>
+        <Form onSubmit={this.handleSubmit}>
+          <Form.Group controlId="translateFrom">
+          <Form.Label>Translate From</Form.Label>
+            <Form.Control as="select" size="sm" onChange={this.handleChange} custom>
+              <option selected>en</option>
+              <option>ru</option>
+            </Form.Control>
+          </Form.Group>
 
-        <Form.Group controlId="translateTo">
-        <Form.Label>Translate To</Form.Label>
-          <Form.Control as="select" size="sm" onChange={this.handleChange} custom>
-            <option>English</option>
-            <option selected>Italian</option>
-          </Form.Control>
-        </Form.Group>
+          <Form.Group controlId="translateTo">
+          <Form.Label>Translate To</Form.Label>
+            <Form.Control as="select" size="sm" onChange={this.handleChange} custom>
+              <option>en</option>
+              <option selected>ru</option>
+            </Form.Control>
+          </Form.Group>
 
-        <InputGroup className="mb-3">
-          <FormControl
-            id="searchWordText"
-            aria-label="Default"
-            aria-describedby="inputGroup-sizing-default"
-            value={this.state.searchWordText}
-            onChange={this.handleChange}
-          />
-          <Button type="submit" className="mb-2">
-            Search
-          </Button>
-        </InputGroup>
-      </Form>
+          <InputGroup className="mb-3">
+            <FormControl
+              id="searchWordText"
+              aria-label="Default"
+              aria-describedby="inputGroup-sizing-default"
+              value={this.state.searchWordText}
+              onChange={this.handleChange}
+            />
+            <Button type="submit" className="mb-2">
+              Search
+            </Button>
+          </InputGroup>
+        </Form>
+
+        <div>
+          <h3>Text: {this.state.translationData.text} </h3>
+          <h3>Translated from language: {this.state.translationData.translateFromLang} </h3>
+          <h3>Translated to language: {this.state.translationData.translateToLang} </h3>
+          <h3>Translated text: {this.state.translationData.translatedText} </h3>
+        </div>
+      </>
+
     )
   }
 }
